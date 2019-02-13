5f07d0638d91f48918524974bdc4b0cec74b29d5
Arturas Budrikas <arthurbud993@gmail.com>
2019-02-13 16:24:09 +0000

    contact

--- script/contact-me.js
+++ script/contact-me.js
@@ -116,7 +116,7 @@ jQuery(document).ready(function($) {
     if (error == true) {
       $("#send_message")
         .queue(function(next) {
-          $(this).html('<object data="http://eliwedel.com/ewpd/demos/jquery-contact-form-v2/error.svg" type="image/svg+xml"></object> Please correct the errors');
+          $(this).html('Please correct the errors');
           next();
           $(this).dequeue();
         })
@@ -128,7 +128,7 @@ jQuery(document).ready(function($) {
         });
     } else if (error == false) {
       $("#send_message")
-        .html('<object data="http://eliwedel.com/ewpd/demos/jquery-contact-form-v2/send.svg" type="image/svg+xml" height="32" style="margin:-10px -5px!important;"></object> Sending')
+        .html('Sending')
         .delay(2000)
         .queue(function(next) {
           $.post("send_email.php", $("#contact_form").serialize(), function(result) {
@@ -136,7 +136,7 @@ jQuery(document).ready(function($) {
             if (r == 1) {
               $("#contact_form")[0].reset();
               $("#send_message")
-                .html('<object data="http://eliwedel.com/ewpd/demos/jquery-contact-form-v2/success.svg" type="image/svg+xml"></object> Message Sent')
+                .html('Message Sent')
                 .delay(5000)
                 .queue(function(next) {
                   $(this).html("Send Message");
@@ -144,7 +144,7 @@ jQuery(document).ready(function($) {
                 });
             } else {
               $("#send_message")
-                .html('<object data="http://eliwedel.com/ewpd/demos/jquery-contact-form-v2/error.svg" type="image/svg+xml"></object> Something went wrong. Please try again later.')
+                .html('Something went wrong. Please try again later.')
                 .delay(5000)
                 .queue(function(next) {
                   $(this).html("Send Message");
